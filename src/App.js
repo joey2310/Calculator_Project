ba2d966df34bf63809776dd76d5bc6db190c9a25
joey2310 <bhagatpratik23@gmail.com>
2024-06-13 00:03:33 +0530

    host

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import {FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+  const [history, setHistory] = useState([]);
+
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        const calculatedResult = eval(input);
+        setResult(calculatedResult);
+        setHistory([...history, { input, result: calculatedResult }]);
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'AC') {
+      setInput('');
+      setResult('');
+    } else if (value === '%') {
+      setInput((prevInput) => {
+        try {
+          setResult(eval(prevInput) / 100);
+          return (eval(prevInput) / 100).toString();
+        } catch (error) {
+          setResult('Error');
+          return '';
+        }
+      });
+    } else if (value === 'Back') {
+      setInput((prevInput) => prevInput.slice(0, -1)); // Remove the last character
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  return (
+    <div className='completeBox'>
+      <div className='cal'>
+        <h1>CALCULATOR</h1>
+      </div>
+      <div className='numberBox'>
+        <input className="input" type="text" value={input} readOnly />
+      </div>
+      <div className='numberBox'>
+        <div>
+          <button className='buttons1' onClick={() => handleClick('AC')}><h2>AC</h2></button>
+          <button className='buttons' onClick={() => handleClick('Back')}><h2> <FontAwesomeIcon icon={['fas', 'arrow-left']} /></h2></button>
+          <button className='buttons' onClick={() => handleClick('%')}><h2><FontAwesomeIcon icon={['fas', 'fa-percent']} /></h2></button>
+          <button className='buttons' onClick={() => handleClick('/')}><h2><FontAwesomeIcon icon={['fas', 'fa-divide']} /></h2></button>
+        </div>
+        <div>
+          <button className='buttons' onClick={() => handleClick('7')}><h2>7</h2></button>
+          <button className='buttons' onClick={() => handleClick('8')}><h2>8</h2></button>
+          <button className='buttons' onClick={() => handleClick('9')}><h2>9</h2></button>
+          <button className='buttons' onClick={() => handleClick('*')}><h2><FontAwesomeIcon icon={['fas', 'fa-xmark']} /></h2></button>
+        </div>
+        <div>
+          <button className='buttons' onClick={() => handleClick('4')}><h2>4</h2></button>
+          <button className='buttons' onClick={() => handleClick('5')}><h2>5</h2></button>
+          <button className='buttons' onClick={() => handleClick('6')}><h2>6</h2></button>
+          <button className='buttons' onClick={() => handleClick('-')}><h2><FontAwesomeIcon icon={['fas', 'fa-minus']} /></h2></button>
+        </div>
+        <div>
+          <button className='buttons' onClick={() => handleClick('1')}><h2>1</h2></button>
+          <button className='buttons' onClick={() => handleClick('2')}><h2>2</h2></button>
+          <button className='buttons' onClick={() => handleClick('3')}><h2>3</h2></button>
+          <button className='buttons' onClick={() => handleClick('+')}><h2><FontAwesomeIcon icon={['fas', 'fa-plus']} /></h2></button>
+        </div>
+        <div>
+          <button className='buttons' onClick={() => handleClick('00')}><h2>00</h2></button>
+          <button className='buttons' onClick={() => handleClick('0')}><h2>0</h2></button>
+          <button className='buttons' onClick={() => handleClick('.')}><h2>.</h2></button>
+          <button className='buttons' type='submit' onClick={() => handleClick('=')}><h2><FontAwesomeIcon icon={['fas', 'fa-equals']} /></h2></button>
+        </div>
+      </div>
+      <div className='numberBox'>
+        <input className="input" type="text" value={result} readOnly />
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Input</th>
+              <th>Result</th>
+            </tr>
+          </thead>
+          <tbody>
+            {history.map((calculation, index) => (
+              <tr key={index}>
+                <td>{calculation.input}</td>
+                <td>{calculation.result}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+library.add(fab, fas, far)
